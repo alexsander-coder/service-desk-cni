ea4cd12efc6d7139288f2959bc4e5201ef551462
Alex Souza <asouza@valeshop.com.br>
2025-12-10 16:53:00 -0300

    fix(accessibility): melhorias de acessibilidade nas telas de listagem e criação de chamados

--- src/app/features/chamados/lista/lista.component.ts
+++ src/app/features/chamados/lista/lista.component.ts
@@ -28,4 +28,18 @@ export class ListaComponent implements OnInit {
       this.chamados = data;
     });
   }
+
+  // ngAfterViewInit() {
+  //   setTimeout(() => {
+  //     const labels = document.querySelectorAll('.p-dropdown-label');
+
+  //     labels.forEach(el => {
+  //       el.setAttribute('role', 'textbox');
+  //       el.setAttribute('aria-haspopup', 'listbox');
+  //       el.setAttribute('aria-expanded', 'false');
+  //       el.setAttribute('aria-label', 'Selecionar quantidade de linhas');
+  //     });
+  //   }, 100);
+  // }
+
 }
